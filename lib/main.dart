c2f3efcae939b233940b203c2b1fee365f9d159a
ChamodJ <Chamod914@gmail.com>
2025-11-18 00:40:51 +0530

    Minor change

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.mainColor),
         scaffoldBackgroundColor: Colors.white,
