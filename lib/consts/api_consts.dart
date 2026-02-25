0bad9b8ff56b105a4a7cc01e1edc53979c438692
ishini21 <ishinishavindhya@gmail.com>
2026-02-25 11:32:49 +0530

    add host url

--- lib/consts/api_consts.dart
+++ lib/consts/api_consts.dart
@@ -1,1 +1,2 @@
-const String backendUrl = 'http://192.168.8.140:5000/api/v1';
+//const String backendUrl = 'http://192.168.8.140:5000/api/v1';
+const String backendUrl = 'https://petbackend-production.up.railway.app/api/v1';
