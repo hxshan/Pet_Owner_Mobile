a86dc8446f86051f57ea2245a77fae98f3210e65
ChamodJ <Chamod914@gmail.com>
2026-04-14 20:46:31 +0530

    Change package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pet_owner_mobile"
+    namespace = "com.pawconnect.pets"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.pet_owner_mobile"
+        applicationId = "com.pawconnect.pets"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
